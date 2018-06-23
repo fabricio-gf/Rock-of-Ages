e44473caef263c7c6484eb7e75849e6beed387a2
guaxininja09 <ginja_gabriel@hotmail.com>
2018-06-23 19:31:39 -0300

    Note prefab
    goodbyeeee moonmaaaaan

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class GameManager : MonoBehaviour {
     public static GameManager instance = null;
+    public ControllerLayout cl;
+    public GameObject[] notes;
 
     private void Awake()
     {
@@ -16,7 +18,66 @@ public class GameManager : MonoBehaviour {
             Destroy(gameObject);
         }
         DontDestroyOnLoad(gameObject);
+
+        Initialize();
     }
+
+    void Initialize(){
+        cl = ControllerLayout.instance;
+        int[] instruments = cl.selectedInstruments;
+        if(cl.currentLayout == 0){
+            for(int i = 0; i < 4; i++){
+                switch (instruments[i])
+                {
+                    case 0:
+                        //disable
+                        break;
+                    case 1:
+                        notes[i].GetComponent<InputBehaviour>().buttonCode = KeyCode.Q;
+                        break;
+                    case 2:
+                        notes[i].GetComponent<InputBehaviour>().buttonCode = KeyCode.Space;
+                        break;
+                    case 3:
+                        notes[i].GetComponent<InputBehaviour>().buttonCode = KeyCode.P;
+                        break;
+                    case 4:
+                        notes[i].GetComponent<InputBehaviour>().buttonCode = KeyCode.KeypadEnter;
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+        }
+        /*else{
+            for(int i = 0; i < 4; i++){
+                switch (instruments[i])
+                {
+                    case 0:
+                        //disable
+                        break;
+                    case 1:
+                        notes[i].GetComponent<InputBehaviour>().buttonCode = KeyCode.;
+                        break;
+                    case 2:
+                        notes[i].GetComponent<InputBehaviour>().buttonCode = KeyCode.;
+                        break;
+                    case 3:
+                        notes[i].GetComponent<InputBehaviour>().buttonCode = KeyCode.;
+                        break;
+                    case 4:
+                        notes[i].GetComponent<InputBehaviour>().buttonCode = KeyCode.;
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }*/
+
+        Destroy(cl.gameObject);
+    }
+
     public void NextBeat()
     {
         //chama os eventos relativos a cada batida;
