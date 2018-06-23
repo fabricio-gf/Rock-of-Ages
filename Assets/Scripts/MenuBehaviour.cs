230dd05a0a6fc88946c8e71aa10576e5ae71e419
Fabricio <fabricio-gf@hotmail.com>
2018-06-23 14:14:04 -0300

    Added end game scene

---
+++ Assets/Scripts/MenuBehaviour.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuBehaviour : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OpenPanel(GameObject panel)
+    {
+        panel.GetComponent<Animator>().SetBool("isOpen", true);
+    }
+
+    public void ClosePanel(GameObject panel)
+    {
+        panel.GetComponent<Animator>().SetBool("isOpen", false);
+    }
+
+    public void ChangeScene(string name){
+        SceneManager.LoadScene(name);
+    }
+}
